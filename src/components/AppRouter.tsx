05e19a1e346b5ceb7695fcfdd09aad30d140057a
Peter Trofimov <ptrofimov7@gmail.com>
2023-02-18 17:38:05 +0200

    fix AppRouter

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 import { Route, Routes } from 'react-router';
 import NotFound from '../pages/NotFound';
 import Index from '../pages/Index';
-import MainLayout from '../layouts/MainLayout';
 
 const AppRouter = () => {
    return (
-      <MainLayout>
-         <Routes>
-            <Route path='/' element={<Index />} />
-            <Route path='*' element={<NotFound />} />
-         </Routes>
-      </MainLayout>
+      <Routes>
+         <Route path='/' element={<Index />} />
+         <Route path='*' element={<NotFound />} />
+      </Routes>
    );
 };
 
