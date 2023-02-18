ffd6ff7cc7fe67260538ec9ef89de5ac6475cd58
Peter Trofimov <ptrofimov7@gmail.com>
2023-02-18 17:39:52 +0200

    fill Converter components

--- src/components/ConverterTable.tsx
+++ src/components/ConverterTable.tsx
@@ -3,7 +3,28 @@ import React from 'react';
 const ConverterTable = () => {
    return (
       <div>
-         Table
+         <table>
+            <thead>
+               <tr>
+                  <th>Currency/Current Date</th>
+                  <th>Buy</th>
+                  <th>Sell</th>
+               </tr>
+            </thead>
+            <tbody>
+               <tr>
+                  <td>USD/UAH</td>
+                  <td>27.5</td>
+                  <td>27.5</td>
+               </tr>
+               <tr>
+               <td>USD/UAH</td>
+                  <td>27.5</td>
+                  <td>27.5</td>
+               </tr>
+
+            </tbody>
+         </table>
       </div>
    );
 };
